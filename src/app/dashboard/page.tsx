1f290651a1f6c33f4e1a26f5d3f01f4b9041da5b
EKVONGPANHARITH <panharitheklover@gmail.com>
2025-05-23 18:33:53 +0700

    init push

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { redirect } from 'next/navigation';
+
+export default function Page(){
+
+    const isLoggedIn = true
+
+    if(isLoggedIn){
+        redirect("dashboard/overview")
+    }
+    else {
+        redirect("/login")
+    }
+}
